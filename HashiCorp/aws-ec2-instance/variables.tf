b0afec57710ed60078aa1c10809d3da2d0b59e38
Deepak <Deepakv@helecloud.com>
2019-03-20 15:14:52 +0000

    testing subfolder

--- HashiCorp/aws-ec2-instance/variables.tf
+++ HashiCorp/aws-ec2-instance/variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "ami_id" {
   description = "ID of the AMI to provision. Default is Ubuntu 14.04 Base Image"
-  default = "ami-0ff7f191316dba328"
+  default = "ami-0ff7f191316dba38"
 }
 
 variable "instance_type" {
